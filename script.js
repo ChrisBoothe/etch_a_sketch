47e7bd8d99492377e80b2d9ff56c6742ea286889
ChrisBoothe <christopher.boothe@live.com>
2019-03-20 14:57:20 -0400

    Add reset grid feature and clean up code

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 
 const container = document.querySelector('.container');
+const btn = document.querySelector('#btn');
+let gridNumber = 0;
 
-let gridNumber = 20;
 
-// Create the grid
-for (var i = 0; i < (gridNumber*gridNumber); i++) {
-    gridSquare = document.createElement('div')
-    gridSquare.className = "square";
-    container.appendChild(gridSquare);
+// Create a grid
+function createGrid(){
+    for (var i = 0; i < (gridNumber*gridNumber); i++) {
+        gridSquare = document.createElement('div')
+        gridSquare.className = "square";
+        container.appendChild(gridSquare);
+    }
 }
 
-const squares = document.querySelectorAll('.square');
+//Remove current grid
+function removeGrid(){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
 // Add event listeners and change color
-squares.forEach((square) => {
-    square.addEventListener('mouseover', (e) => {
-        e.target.style.background = 'black'
+function colorSquares(){
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            e.target.style.background = 'black'
+        });
     });
-});
+}
 
+// Erase and create new grid via button
+btn.addEventListener('click', () => {
+    removeGrid();
+    gridNumber = prompt("How many squares?");
+    container.style.gridTemplateColumns = "repeat("+gridNumber+", 1fr)";
+    container.style.gridTemplateRows = "repeat("+gridNumber+", 1fr)";
+    createGrid();
+    colorSquares();
+});
 
 
 
